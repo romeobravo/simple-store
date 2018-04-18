2ad42a4927fdb81cc7376f8f947f7515d0a87aad
Ruben Buitelaar <r.buitelaar@websend.nl>
2018-04-18 12:02:02 +0200

    Add retrieval method

--- index.js
+++ index.js
@@ -4,35 +4,64 @@ const fs = require('fs')
 const Koa = require('koa')
 const koaBody = require('koa-body')
 const cors = require('@koa/cors')
+const uuid = require('uuid/v1');
 
 const createStore = file => {
-  fs.exists(file, exists => {
-    if (!exists) {
-      fs.writeFile(file)
-    }
+  return new Promise((resolve, reject) => {  
+    fs.exists(file, exists => {
+      if (!exists) {
+        fs.writeFileSync(file)
+      }
+      resolve()
+    })
   })
 }
 
-const save = (key, entry) => {
+const save = async (entry) => {
+  const store = process.env.STORE_FILE || 'db.json'
+  await createStore(store)
+
+  return new Promise((resolve, reject) => {
+    fs.readFile(store, (err, data) => {
+      if (err) return
+
+      let json
+      try {
+        json = JSON.parse(data)
+      } catch (e) {
+        json = {}
+      }
+
+      const id = uuid()
+      json = json || {}
+      json[id] = entry
+
+      fs.writeFileSync(store, JSON.stringify(json))
+      
+      resolve(id)
+    })
+  })
+}
+
+const get = async (key) => {
   const store = process.env.STORE_FILE || 'db.json'
   createStore(store)
 
-  fs.readFile(store, (err, data) => {
-    if (err) return
-
-    let json
-    try {
-      json = JSON.parse(data)
-    } catch (e) {
-      json = {}
-    }
-
-    json = json || {}
-    json[key] = json[key] || []
-    json[key].push(entry)
-
-    fs.writeFileSync(store, JSON.stringify(json))
+  return new Promise((resolve, reject) => {
+    fs.readFile(store, (err, data) => {
+      if (err) return
+      
+      let json
+      try {
+        json = JSON.parse(data)
+      } catch (e) {
+        json = {}
+      }
+      
+      resolve(json[key])
+    })  
   })
+  
 }
 
 const app = new Koa()
@@ -40,10 +69,15 @@ app.use(cors())
 app.use(koaBody())
 
 app.use(async ctx => {
-  ctx.body = { success: true }
 
   if (ctx.request.body.type) {
-    save(ctx.request.body.type, ctx.request.body)
+    const id = await save(ctx.request.body)
+    console.log(id);
+    ctx.body = { success: true, id: id }
+  }
+
+  if (ctx.request.body.id) {
+    ctx.body = await get(ctx.request.body.id)
   }
 })
 
